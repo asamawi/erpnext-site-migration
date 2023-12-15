5e799cc2a4f7162153c0196a55565a987472ca7b
Ahmad Samawi <asamawi@n4s-solutions.com>
2023-12-15 23:42:46 +0300

    Update clean backup to delete files on new server

--- migration_script.sh
+++ migration_script.sh
@@ -110,8 +110,14 @@ copy_encryption_key() {
     check_success "Copying Encryption Key"
 }
 
+# Function to clean up backups
 clean_backup() {
+    # Remove all files inside the backups directory on the old server
     ssh frappe@$old_server "rm -rf ~/frappe-bench/sites/$old_site/private/backups/*"
+
+    # Enable extended globbing and remove all files except 'files' and 'backups' directories on the new server
+    shopt -s extglob
+    ssh frappe@$new_server "rm -rf !(files|backups) ~/frappe-bench/sites/$new_site/private/"
 
     check_success "Cleaning Backup"
 }
